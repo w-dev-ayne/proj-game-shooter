749b201c107f3a4d4bcc2a36e4757b56a0f70b40
Wayne <w.dev.ayne@gmail.com>
2025-01-14 18:54:37 +0900

    feat : UI Appear 스크립트 구현

---
+++ Assets/Scripts/UI/UIAppearAnimA.cs
@@ -0,0 +1,33 @@
+using System;
+using DG.Tweening;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class UIAppearAnimA : MonoBehaviour
+{
+    [SerializeField] private bool playOnAwake = false;
+
+    public void Play()
+    {
+        this.transform.localScale = Vector3.one * 0.8f;
+        this.transform.DOScale(Vector3.one * 1.0f, 0.5f).SetEase(Ease.OutBounce);
+    }
+    
+    private void OnEnable()
+    {
+        if (playOnAwake)
+            Play();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
