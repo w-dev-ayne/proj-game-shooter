e9a5a5282bfe31ca1afe584bd5ed5689cefbaf73
Wayne <w.dev.ayne@gmail.com>
2025-02-18 22:44:58 +0900

    feat : loading canvas 구현

---
+++ Assets/Scripts/UI/UI_Loading.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class UI_Loading : UI_Popup
+{
+    public override bool Init()
+    {
+        
+        if (base.Init() == false)
+            return false;
+        return true;
+    }
+}
